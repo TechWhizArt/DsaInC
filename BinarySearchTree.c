63a61d9e7c2807d00a972c625570e6ee8a328784
Niharika <niharika.mailme123@gmail.com>
2025-01-10 18:24:01 +0530

    ea

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -30,6 +30,66 @@ node *insert(node *root, int data) {
     // Return the unchanged root node
     return root;
 }
+
+node *minValueNode(node *root) {
+    node *current = root;
+    while (current && current->left){
+        current = current->left;
+    }
+    return current;
+}
+
+
+
+node *deleteNode(node *root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+    // Traverse the tree to find the node to delete
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        // Node with only one child or no child
+        if (root->left == NULL) {
+            node *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // Node with two children: Get the inorder successor
+        node *temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+
+    return root;
+}
+
+node *search (node *root, int info){
+    if(root->data == info || root == NULL) {
+        return root;
+    }else if (info < root->data) {
+        return search(root->left, info);
+    }else return search(root->right, info);
+}
+void inorderTraversal(node *root) {
+    if(root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d->", root->data);
+        inorderTraversal(root->right);
+    }
+
+    
+}
+
+
+
 int main() {
     struct Node* root = NULL;
     int keys[] = {50, 30, 70, 20, 40, 60, 80}; // Example keys to insert
@@ -43,7 +103,11 @@ int main() {
     // Print the inorder traversal of the BST
     printf("Inorder traversal of the BST: ");
     inorderTraversal(root);
-    printf("\n");
+    printf("\n\n\n");
+    node * p = search(root, 50);
+    printf("%d , %d\n\n\n", p, p->data);
+    deleteNode(root, 40);
 
+    inorderTraversal(root);
     return 0;
 }
