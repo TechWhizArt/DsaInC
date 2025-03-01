24bbdf818183b9010bd59359536a25c5715e3a94
Niharika <niharika.mailme123@gmail.com>
2025-03-01 12:14:04 +0530

    added

--- sorting.c
+++ sorting.c
@@ -299,85 +299,118 @@
 
 
 
-// //Merge sort...
+// // //Merge sort...
+
+// #include <stdio.h>
+
+// // Function to merge two subarrays
+// void merge(int arr[], int left, int mid, int right) {
+//     int i=left;
+//     int j=mid+1;
+//     int k=left;
+//     while(i<=mid && j<=right) {
+//         if(arr[i]<=arr[j]) {
+//             arr[k] = arr[i];
+//             i++;
+
+//         }else {
+//             arr[k] = arr[j];
+//             j++;
+
+//         }
+//         k++;
+//     }
+//     while(i<=mid) {
+//         arr[k] = arr[i];
+//         i++;
+//         k++;
+//     }
+//     while(j<=right) {
+//         arr[k] = arr[j];
+//         j++;
+//         k++;
+//     }
+// }
+
+// // Recursive function for Merge Sort
+// void mergeSort(int arr[], int left, int right) {
+//     if (left < right) {
+//         int mid = left + (right - left) / 2;
+
+//         // Recursively divide the array
+//         mergeSort(arr, left, mid);
+//         mergeSort(arr, mid + 1, right);
+
+//         // Merge the sorted halves
+//         merge(arr, left, mid, right);
+//     }
+// }
+
+// // Function to print the array
+// void printArray(int arr[], int size) {
+//     for (int i = 0; i < size; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+// }
+
+// // Main function
+// int main() {
+//     int arr[] = {12, 11, 13, 5, 6, 7};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+
+//     printf("Original array: ");
+//     printArray(arr, size);
+
+//     mergeSort(arr, 0, size - 1);
+
+//     printf("Sorted array: ");
+//     printArray(arr, size);
+
+//     return 0;
+// }
+
+
+// Quick sort...
 
 #include <stdio.h>
+#include <stdlib.h>
 
-// Function to merge two subarrays
-void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+int partition (int arr[], int left, int right){
 
-    // Temporary arrays
-    int L[n1], R[n2];
-
-    // Copy data to temporary arrays
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    // Merge the two arrays back into arr[]
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+    int pivot = arr[left];
+    int i = left;
+    int j = right;
+    while(i<j) {
+        do {
             i++;
-        } else {
-            arr[k] = R[j];
-            j++;
+        } while(arr[i]<=pivot);
+        do {
+            j--;
+        } while(arr[j]>pivot);
+
+        if(i<j) {
+            swap(&arr[i], &arr[j]);
         }
-        k++;
     }
-
-    // Copy remaining elements of L[]
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy remaining elements of R[]
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    swap(&arr[left], &arr[j]);
+    return j;
+}
+void quickSort(int arr[], int left, int right) {
+    if(left<right) {
+        int j = partition(arr, left, right);
+        quickSort(arr, left, j);
+        quickSort(arr, j+1, right);
     }
 }
 
-// Recursive function for Merge Sort
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        // Recursively divide the array
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        // Merge the sorted halves
-        merge(arr, left, mid, right);
-    }
-}
-
-// Function to print the array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// Main function
-int main() {
+int main () {
     int arr[] = {12, 11, 13, 5, 6, 7};
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    printArray(arr, size);
-
-    mergeSort(arr, 0, size - 1);
-
-    printf("Sorted array: ");
-    printArray(arr, size);
-
+    int size = sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr, 0, size-1);
+    for(int i = 0; i<size; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
+
+
